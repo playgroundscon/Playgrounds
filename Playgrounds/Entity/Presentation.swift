eae876a9cf4a3a28fbdc124fe24371d2f55e73ff
Bas Broek <bas@basbroek.nl>
2017-01-04 22:55:38 +0100

    Put JSON inits in extensions

--- Playgrounds/Entity/Presentation.swift
+++ Playgrounds/Entity/Presentation.swift
@@ -18,6 +18,9 @@ struct Presentation {
         self.title = "Swift eye for the Stringly typed API"
         self.blurb = "With the upcoming release of the third major version of Swift, massive improvements are coming to the language and we are beginning to see the chains being broken on some of shackles of Objective-C’s legacy.\n\nHowever a lot of these improvements still rely on “Stringly typed” APIs which have the potential to trip us up when developing apps. In this talk we are going to look into how we can avoid using these APIs by replacing them with alternatives that make our code more readable, safer, intentional and Swifty."
     }
+}
+
+extension Presentation {
     
     init?(json: JSON) {
         guard
